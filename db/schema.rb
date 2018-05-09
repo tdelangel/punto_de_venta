09123445af626abd04a304c4a0b82213b3a3767b
Your Name <you@example.com>
2018-05-09 17:51:49 -0500

    mysql

--- db/schema.rb
+++ db/schema.rb
@@ -14,66 +14,70 @@
 ActiveRecord::Schema.define(version: 6) do
 
   create_table "clients", force: :cascade do |t|
-    t.string   "cedula",     null: false
-    t.string   "nombres",    null: false
-    t.string   "direccion"
-    t.string   "telefono"
-    t.string   "correo"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string   "cedula",     limit: 255, null: false
+    t.string   "nombres",    limit: 255, null: false
+    t.string   "direccion",  limit: 255
+    t.string   "telefono",   limit: 255
+    t.string   "correo",     limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
   end
 
   create_table "products", force: :cascade do |t|
-    t.string   "nombre",     null: false
-    t.decimal  "precio",     null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string   "nombre",     limit: 255,                null: false
+    t.decimal  "precio",                 precision: 10, null: false
+    t.datetime "created_at",                            null: false
+    t.datetime "updated_at",                            null: false
   end
 
   create_table "sale_details", force: :cascade do |t|
-    t.integer  "sale_id"
-    t.integer  "product_id"
-    t.integer  "cantidad",   null: false
-    t.decimal  "total",      null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.integer  "sale_id",    limit: 4
+    t.integer  "product_id", limit: 4
+    t.integer  "cantidad",   limit: 4,                null: false
+    t.decimal  "total",                precision: 10, null: false
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
   end
 
-  add_index "sale_details", ["product_id"], name: "index_sale_details_on_product_id"
-  add_index "sale_details", ["sale_id"], name: "index_sale_details_on_sale_id"
+  add_index "sale_details", ["product_id"], name: "index_sale_details_on_product_id", using: :btree
+  add_index "sale_details", ["sale_id"], name: "index_sale_details_on_sale_id", using: :btree
 
   create_table "sales", force: :cascade do |t|
     t.date     "fecha"
-    t.decimal  "subtotal"
-    t.decimal  "total"
-    t.integer  "client_id",  null: false
-    t.integer  "user_id",    null: false
-    t.boolean  "estado"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.decimal  "subtotal",             precision: 10
+    t.decimal  "total",                precision: 10
+    t.integer  "client_id",  limit: 4,                null: false
+    t.integer  "user_id",    limit: 4,                null: false
+    t.boolean  "estado",     limit: 1
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
   end
 
-  add_index "sales", ["client_id"], name: "index_sales_on_client_id"
-  add_index "sales", ["user_id"], name: "index_sales_on_user_id"
+  add_index "sales", ["client_id"], name: "index_sales_on_client_id", using: :btree
+  add_index "sales", ["user_id"], name: "index_sales_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
-    t.string   "nombres",                      null: false
-    t.string   "direccion"
-    t.string   "telefono"
-    t.boolean  "sexo"
-    t.boolean  "activo"
-    t.text     "observacion"
-    t.string   "foto"
-    t.string   "email",                        null: false
-    t.string   "crypted_password"
-    t.string   "salt"
+    t.string   "nombres",                      limit: 255,   null: false
+    t.string   "direccion",                    limit: 255
+    t.string   "telefono",                     limit: 255
+    t.boolean  "sexo",                         limit: 1
+    t.boolean  "activo",                       limit: 1
+    t.text     "observacion",                  limit: 65535
+    t.string   "foto",                         limit: 255
+    t.string   "email",                        limit: 255,   null: false
+    t.string   "crypted_password",             limit: 255
+    t.string   "salt",                         limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "remember_me_token"
+    t.string   "remember_me_token",            limit: 255
     t.datetime "remember_me_token_expires_at"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["remember_me_token"], name: "index_users_on_remember_me_token"
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["remember_me_token"], name: "index_users_on_remember_me_token", using: :btree
 
+  add_foreign_key "sale_details", "products"
+  add_foreign_key "sale_details", "sales"
+  add_foreign_key "sales", "clients"
+  add_foreign_key "sales", "users"
 end
